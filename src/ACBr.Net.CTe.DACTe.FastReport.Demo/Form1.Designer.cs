78c39374618a05e07353f6dc8ba1725ec51a12d2
Marcos Gerene <mgf.spirito@gmail.com>
2018-10-20 10:35:49 -0300

    DacteRetrato

---
+++ src/ACBr.Net.CTe.DACTe.FastReport.Demo/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace ACBr.Net.CTe.DACTe.FastReport.Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chbQuebrarLinhaObservacao = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.txtArquivo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtSoftwareHouse = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pcbLogotipo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSite = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogotipo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chbQuebrarLinhaObservacao
+            // 
+            this.chbQuebrarLinhaObservacao.AutoSize = true;
+            this.chbQuebrarLinhaObservacao.Location = new System.Drawing.Point(227, 146);
+            this.chbQuebrarLinhaObservacao.Name = "chbQuebrarLinhaObservacao";
+            this.chbQuebrarLinhaObservacao.Size = new System.Drawing.Size(163, 17);
+            this.chbQuebrarLinhaObservacao.TabIndex = 41;
+            this.chbQuebrarLinhaObservacao.Text = "Quebrar linha na observação";
+            this.chbQuebrarLinhaObservacao.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(225, 204);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 54);
+            this.groupBox1.TabIndex = 40;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = " Eventos ";
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(200, 18);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(83, 28);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "Design";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(104, 18);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(83, 28);
+            this.button8.TabIndex = 23;
+            this.button8.Text = "Imprimr";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(7, 18);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(83, 28);
+            this.button9.TabIndex = 22;
+            this.button9.Text = "Visualizar";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(498, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(27, 22);
+            this.button6.TabIndex = 39;
+            this.button6.Text = "...";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // txtArquivo
+            // 
+            this.txtArquivo.Location = new System.Drawing.Point(227, 120);
+            this.txtArquivo.Name = "txtArquivo";
+            this.txtArquivo.Size = new System.Drawing.Size(269, 20);
+            this.txtArquivo.TabIndex = 38;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Arquivo .frx";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(416, 169);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 28);
+            this.button5.TabIndex = 36;
+            this.button5.Text = "Design";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(324, 169);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 28);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Imprimr";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSoftwareHouse
+            // 
+            this.txtSoftwareHouse.Location = new System.Drawing.Point(227, 30);
+            this.txtSoftwareHouse.Name = "txtSoftwareHouse";
+            this.txtSoftwareHouse.Size = new System.Drawing.Size(298, 20);
+            this.txtSoftwareHouse.TabIndex = 34;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 33;
+            this.label2.Text = "Software House";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(96, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 23);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "X";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(65, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(27, 23);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 30;
+            this.label1.Text = "Logotipo";
+            // 
+            // pcbLogotipo
+            // 
+            this.pcbLogotipo.Location = new System.Drawing.Point(9, 31);
+            this.pcbLogotipo.Name = "pcbLogotipo";
+            this.pcbLogotipo.Size = new System.Drawing.Size(202, 133);
+            this.pcbLogotipo.TabIndex = 29;
+            this.pcbLogotipo.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(227, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 28);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Visualizar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSite
+            // 
+            this.txtSite.Location = new System.Drawing.Point(227, 75);
+            this.txtSite.Name = "txtSite";
+            this.txtSite.Size = new System.Drawing.Size(298, 20);
+            this.txtSite.TabIndex = 43;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 13);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Site";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 267);
+            this.Controls.Add(this.txtSite);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chbQuebrarLinhaObservacao);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.txtArquivo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtSoftwareHouse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pcbLogotipo);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogotipo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chbQuebrarLinhaObservacao;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox txtArquivo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtSoftwareHouse;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pcbLogotipo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSite;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
